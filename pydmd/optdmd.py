99742bfbf6abddaf4cbe5f4178fadd5168a22abc
Unknown <loiseau.jc@gmail.com>
2019-04-17 14:21:48 +0200

    Clean-ish implementation of the algorithm by Héas & Herzet
    - The optDMD object is now more compliant with the structure of PyDMD.
    - Still need to implement some sanity checks.

--- pydmd/optdmd.py
+++ pydmd/optdmd.py
@@ -1,18 +1,19 @@
+# --> Import PyDMD base class for DMD.
+from .dmdbase import DMDBase
+"""
+Dynamic Mode Decomposition
+"""
+# Author : Jean-Christophe Loiseau <jean-christophe.loiseau@ensam.eu>
 #
-#
-#
-#
-#
+# Licence : Whatever licence PyDMD is using.
 
 # --> Import standard python packages
 import numpy as np
 from scipy.linalg import eig, eigvals
 from scipy.linalg import pinv2
 
-pinv = lambda x: pinv2(x, rcond=-1)
 
-# --> Import PyDMD base class for DMD.
-from .dmdbase import DMDBase
+def pinv(x): return pinv2(x, rcond=10*np.finfo(float).eps)
 
 
 class optDMD(DMDBase):
@@ -22,36 +23,44 @@ class optDMD(DMDBase):
 
     This class implements the closed-form solution to the DMD minimization
     problem. It relies on the optimal solution and the demonstration given by
-    P. Héas & C. Herzet [1].
+    Héas & Herzet [1].
 
+    Parameters
+    ----------
+    factorization : string
+        Compute either the eigenvalue decomposition of the unknown high-
+        -dimensional DMD operator (factorization="evd") or its singular value
+        decomposition, i.e. factorization="svd". (the default is "svd").
+
+    svd_rank : int or float
+        The rank for the truncation; If 0, the method computes the
+        optimal rank and uses it for truncation; if positive interger, the
+        method uses the argument for the truncation; if float between 0 and 1,
+        the rank is the number of the biggest singular values that are needed
+        to reach the 'energy' specified by `svd_rank`; if -1, the method does
+        not compute truncation.
+
+    Attributes
+    -----------
+    ???
+
+    References
+    ----------
     [1] P. Héas & C. Herzet. Low-rank dynamic mode decomposition: optimal
     solution in polynomial time. arXiv:1610.02962. 2016
 
     """
 
     def __init__(self, factorization="svd", svd_rank=0):
-        """Initialize the DMD model.
-
-        Parameters
-        ----------
-        factorization : string
-            Compute either the eigenvalue decomposition of the unknown high-
-            -dimensional DMD operator (factorization="evd") or its singular value
-            decomposition, i.e. factorization="svd". (the default is "svd").
-
-        Returns
-        -------
-        self
-            The instantiate DMD model.
-
-        """
 
         # --> Compute either the SVD or EVD factorization of the DMD problem.
         self.factorization = factorization
+
+        # --> Rank of the DMD model.
         self.svd_rank = svd_rank
 
     def fit(self, X, Y=None):
-        """Fit the DMD model using the input X and output Y.
+        """Fit the DMD model using the input X and output Y (if provided).
 
         Parameters
         ----------
@@ -62,8 +71,8 @@ class optDMD(DMDBase):
 
         Returns
         -------
-        type
-            Description of returned object.
+        self : optDMD object
+            The fitted DMD model.
 
         """
 
@@ -74,12 +83,12 @@ class optDMD(DMDBase):
             X = X[:, :-1]   # x = x[k]
 
         # --> Compute the singular value decomposition of X.
-        Ux, Sx, Vx = self._compute_svd(X, X.shape[1])
+        Ux, Sx, Vx = self._compute_svd(X, -1)
 
         # --> Compute the matrix Z.
         Z = np.linalg.multi_dot(
             [Y, Vx, np.diag(Sx), pinv(np.diag(Sx)), Vx.conj().T]
-            )
+        )
 
         # --> Compute the singular value decomposition of Z.
         Uz, _, _ = self._compute_svd(Z, self.svd_rank)
@@ -93,11 +102,17 @@ class optDMD(DMDBase):
         S = Q.T.dot(Uz)
 
         if self.factorization == "svd":
+            # --> Input/Output spaces.
+            self._input_space = Q
+            self._output_space = Uz
+
             # --> DMD-SVD modes.
             self._modes = Uz
+
             # --> Low-dimensional DMD operator.
             self._Atilde = S
-            # --> Eigenvalues of S.
+
+            # --> Eigenvalues of Atilde.
             self._eigs = eigvals(S)
 
         elif self.factorization == "evd":
@@ -108,48 +123,57 @@ class optDMD(DMDBase):
             right_vecs = np.linalg.multi_dot(
                 [Uz, Q.T, Uz, vecs_right]
             )
-            right_vecs[:, abs(vals)>1e-6] /= vals[abs(vals)>1e-6]
+            right_vecs[:, abs(vals) > 1e-8] /= vals[abs(vals) > 1e-8]
 
             # --> Build the matrix of left eigenvectors.
             left_vecs = np.linalg.multi_dot(
                 [Q, vecs_left]
             )
-            left_vecs[:, abs(vals)>1e-6] /= vals[abs(vals)>1e-6]
+            left_vecs[:, abs(vals) > 1e-8] /= vals[abs(vals) > 1e-8]
 
             # --> Rescale the left eigenvectors.
             m = np.diag(left_vecs.T.conj().dot(right_vecs))
-            left_vecs[:, abs(vals)>1e-6] /= m[abs(vals)>1e-6].conj()
+            left_vecs[:, abs(vals) > 1e-8] /= m[abs(vals) > 1e-8].conj()
 
+            # --> Input/Output modes.
             self._input_space = left_vecs
             self._output_space = right_vecs
-            self._Atilde = np.diag(vals)
+
+            # --> DMD-EVD modes.
+            self._modes = right_vecs
+
+            # --> Low-dimensional DMD operator.
+            self._Atilde = S
+
+            # --> Eigenvalues of Atilde.
             self._eigs = vals
 
         return self
 
     def predict(self, X):
-        """Short summary.
+        """Predict the output Y given the input X using the fitted DMD model.
 
         Parameters
         ----------
         X : numpy array
-            Description of parameter `X`.
+            Input data.
 
         Returns
         -------
         Y : numpy array
-            Description of returned object `Y`.
+            Predicted output.
 
         """
 
         # --> Factorization : SVD.
         if self.factorization == "svd":
             Y = np.linalg.multi_dot(
-                [self._modes, self._Atilde, self._modes.T.conj(), X]
+                [self._output_space, self._input_space.T.conj(), X]
             )
+        # --> Factorization : EVD.
         elif self.factorization == "evd":
-            raise NotImplementedError(
-                "Prediction using the EVD factorization is not implemented yet."
-                )
+            Y = np.linalg.multi_dot(
+                [self._output_space, np.diag(self._eigs), self._input_space.T.conj(), X]
+            )
 
         return Y
