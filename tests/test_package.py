990650c048beb84cb593947d508b00109400e55f
Nicola Demo <demo.nicola@gmail.com>
2021-01-22 16:58:48 +0100

    Remove old tests

--- tests/test_package.py
+++ tests/test_package.py
@@ -6,23 +6,6 @@ from os import path
 
 
 class TestPackage(TestCase):
-    def test_modules_name(self):
-        # it checks that __all__ includes all the .py files in dmd folder
-        import pydmd
-        package = pydmd
-
-        f_aux = []
-        for (__, __, filenames) in walk('pydmd'):
-            f_aux.extend(filenames)
-
-        f = []
-        for i in f_aux:
-            file_name, file_ext = path.splitext(i)
-            if file_name != '__init__' and file_ext == '.py':
-                f.append(file_name)
-
-        assert (sorted(package.__all__) == sorted(f))
-
     def test_import_dm_1(self):
         import pydmd as dm
         dmd = dm.dmd.DMDBase()
