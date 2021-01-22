1645dc8cadb5bcd90bf8b5e85db8445a2e5c169b
Nicola Demo <demo.nicola@gmail.com>
2021-01-22 15:31:11 +0100

    Fix dependencies installation via setup.py

--- pydmd/__init__.py
+++ pydmd/__init__.py
@@ -3,16 +3,8 @@ PyDMD init
 """
 __all__ = ['dmdbase', 'dmd', 'fbdmd', 'mrdmd', 'cdmd', 'hodmd', 'dmdc', 'optdmd']
 
-__title__ = "pydmd"
-__author__ = "Nicola Demo, Marco Tezzele"
-__copyright__ = "Copyright 2017-2019, PyDMD contributors"
-__license__ = "MIT"
-__version__ = "0.3.1"
-__mail__ = 'demo.nicola@gmail.com, marcotez@gmail.com'
-__maintainer__ = __author__
-__status__ = "Stable"
 
-
+from .meta import *
 from .dmdbase import DMDBase
 from .dmd import DMD
 from .fbdmd import FbDMD
---
+++ pydmd/meta.py
@@ -0,0 +1,20 @@
+__all__ = [
+    '__project__',
+    '__title__',
+    '__author__',
+    '__copyright__',
+    '__license__',
+    '__version__',
+    '__mail__',
+    '__maintainer__',
+    '__status__']
+
+
+__title__ = "pydmd"
+__author__ = "Nicola Demo, Marco Tezzele"
+__copyright__ = "Copyright 2017-2019, PyDMD contributors"
+__license__ = "MIT"
+__version__ = "0.3.1"
+__mail__ = 'demo.nicola@gmail.com, marcotez@gmail.com'
+__maintainer__ = __author__
+__status__ = "Stable"
