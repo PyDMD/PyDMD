3b4a6fb0f0a5cb6cb510e143c869434ffb51818d
sichinaga <56459460+sichinaga@users.noreply.github.com>
2022-11-03 14:41:48 -0700

    Added rank computation and tests

--- pydmd/rdmd.py
+++ pydmd/rdmd.py
@@ -10,6 +10,44 @@ Systems, 18, 2019.
 import numpy as np
 from .cdmd import CDMD
 
+def compute_rank(X, svd_rank=0):
+    """
+    Rank computation for the truncated Singular Value Decomposition.
+    :param numpy.ndarray X: the matrix to decompose.
+    :param svd_rank: the rank for the truncation; If 0, the method computes
+        the optimal rank and uses it for truncation; if positive interger,
+        the method uses the argument for the truncation; if float between 0
+        and 1, the rank is the number of the biggest singular values that
+        are needed to reach the 'energy' specified by `svd_rank`; if -1,
+        the method does not compute truncation. Default is 0.
+    :type svd_rank: int or float
+    :return: the computed rank truncation.
+    :rtype: int
+    References:
+    Gavish, Matan, and David L. Donoho, The optimal hard threshold for
+    singular values is, IEEE Transactions on Information Theory 60.8
+    (2014): 5040-5053.
+    """
+    U, s, V = np.linalg.svd(X, full_matrices=False)
+
+    def omega(x):
+        return 0.56 * x**3 - 0.95 * x**2 + 1.82 * x + 1.43
+
+    if svd_rank == 0:
+        beta = np.divide(*sorted(X.shape))
+        tau = np.median(s) * omega(beta)
+        rank = np.sum(s > tau)
+    elif 0 < svd_rank < 1:
+        cumulative_energy = np.cumsum(s**2 / (s**2).sum())
+        rank = np.searchsorted(cumulative_energy, svd_rank) + 1
+    elif svd_rank >= 1 and isinstance(svd_rank, int):
+        rank = min(svd_rank, U.shape[1])
+    else:
+        rank = X.shape[1]
+
+    return rank
+
+
 class RDMD(CDMD):
     """
     Randomized Dynamic Mode Decomposition
@@ -22,9 +60,9 @@ class RDMD(CDMD):
     """
     def __init__(
         self,
-        svd_rank,
         oversampling=10,
         power_iters=2,
+        svd_rank=0,
         tlsq_rank=0,
         opt=False,
         rescale_mode=None,
@@ -57,6 +95,9 @@ class RDMD(CDMD):
         """
         # Perform the Randomized QB Decomposition
         m = self._snapshots.shape[1]
+
+        # Compute the target rank
+        self._svd_rank = compute_rank(self._snapshots, self._svd_rank)
 
         # Generate random test matrix (with slight oversampling)
         Omega = np.random.randn(m, self._svd_rank + self._oversampling)
--- tests/test_rdmd.py
+++ tests/test_rdmd.py
@@ -11,7 +11,6 @@ import os
 # f2 = lambda x,t: (sech(x)*np.tanh(x))*(2.*np.exp(1j*2.8*t))
 sample_data = np.load('tests/test_datasets/input_sample.npy')
 
-
 def create_noisy_data():
     mu = 0.
     sigma = 0.  # noise standard deviation
@@ -32,11 +31,10 @@ def create_noisy_data():
 noisy_data = create_noisy_data()
 
 
-# def test_shape():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD(svd_rank=-1)
-#     dmd.fit(X=[d for d in sample_data.T])
-#     assert dmd.modes.shape[1] == sample_data.shape[1] - 1
+def test_shape():
+    dmd = RDMD(svd_rank=-1)
+    dmd.fit(X=[d for d in sample_data.T])
+    assert dmd.modes.shape[1] == sample_data.shape[1] - 1
 
 def test_truncation_shape():
     dmd = RDMD(svd_rank=3)
@@ -48,11 +46,10 @@ def test_Atilde_shape():
     dmd.fit(X=sample_data)
     assert dmd.atilde.shape == (dmd.svd_rank, dmd.svd_rank)
 
-# def test_eigs_1():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD(svd_rank=-1)
-#     dmd.fit(X=sample_data)
-#     assert len(dmd.eigs) == 14
+def test_eigs_1():
+    dmd = RDMD(svd_rank=-1)
+    dmd.fit(X=sample_data)
+    assert len(dmd.eigs) == 14
 
 def test_eigs_2():
     dmd = RDMD(svd_rank=5)
@@ -71,12 +68,11 @@ def test_dynamics_1():
     dmd.fit(X=sample_data)
     assert dmd.dynamics.shape == (5, sample_data.shape[1])
 
-# def test_reconstructed_data():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     dmd_data = dmd.reconstructed_data
-#     np.testing.assert_allclose(dmd_data, sample_data)
+def test_reconstructed_data():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    dmd_data = dmd.reconstructed_data
+    np.testing.assert_allclose(dmd_data, sample_data)
 
 def test_original_time():
     dmd = RDMD(svd_rank=2)
@@ -84,12 +80,11 @@ def test_original_time():
     expected_dict = {'dt': 1, 't0': 0, 'tend': 14}
     np.testing.assert_equal(dmd.original_time, expected_dict)
 
-# def test_original_timesteps():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     np.testing.assert_allclose(dmd.original_timesteps,
-#                                 np.arange(sample_data.shape[1]))
+def test_original_timesteps():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    np.testing.assert_allclose(dmd.original_timesteps,
+                                np.arange(sample_data.shape[1]))
 
 def test_dmd_time_1():
     dmd = RDMD(svd_rank=2)
@@ -97,120 +92,105 @@ def test_dmd_time_1():
     expected_dict = {'dt': 1, 't0': 0, 'tend': 14}
     np.testing.assert_equal(dmd.dmd_time, expected_dict)
 
-# def test_dmd_time_2():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     dmd.dmd_time['t0'] = 10
-#     dmd.dmd_time['tend'] = 14
-#     expected_data = sample_data[:, -5:]
-#     np.testing.assert_allclose(dmd.reconstructed_data, expected_data)
+def test_dmd_time_2():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    dmd.dmd_time['t0'] = 10
+    dmd.dmd_time['tend'] = 14
+    expected_data = sample_data[:, -5:]
+    np.testing.assert_allclose(dmd.reconstructed_data, expected_data)
 
-# def test_dmd_time_3():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     dmd.dmd_time['t0'] = 8
-#     dmd.dmd_time['tend'] = 11
-#     expected_data = sample_data[:, 8:12]
-#     np.testing.assert_allclose(dmd.reconstructed_data, expected_data)
+def test_dmd_time_3():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    dmd.dmd_time['t0'] = 8
+    dmd.dmd_time['tend'] = 11
+    expected_data = sample_data[:, 8:12]
+    np.testing.assert_allclose(dmd.reconstructed_data, expected_data)
 
-# def test_plot_eigs_1():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     dmd.plot_eigs(show_axes=True, show_unit_circle=True)
-#     plt.close()
+def test_plot_eigs_1():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    dmd.plot_eigs(show_axes=True, show_unit_circle=True)
+    plt.close()
 
-# def test_plot_eigs_2():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     dmd.plot_eigs(show_axes=False, show_unit_circle=False)
-#     plt.close()
+def test_plot_eigs_2():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    dmd.plot_eigs(show_axes=False, show_unit_circle=False)
+    plt.close()
 
-# def test_plot_modes_1():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     with raises(ValueError):
-#         dmd.plot_modes_2D()
+def test_plot_modes_1():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    with raises(ValueError):
+        dmd.plot_modes_2D()
 
-# def test_plot_modes_2():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD(svd_rank=-1)
-#     dmd.fit(X=sample_data)
-#     dmd.plot_modes_2D((1, 2, 5), x=np.arange(20), y=np.arange(20))
-#     plt.close()
+def test_plot_modes_2():
+    dmd = RDMD(svd_rank=-1)
+    dmd.fit(X=sample_data)
+    dmd.plot_modes_2D((1, 2, 5), x=np.arange(20), y=np.arange(20))
+    plt.close()
 
-# def test_plot_modes_3():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
-#     dmd.fit(X=snapshots)
-#     dmd.plot_modes_2D()
-#     plt.close()
+def test_plot_modes_3():
+    dmd = RDMD()
+    snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
+    dmd.fit(X=snapshots)
+    dmd.plot_modes_2D()
+    plt.close()
 
-# def test_plot_modes_4():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
-#     dmd.fit(X=snapshots)
-#     dmd.plot_modes_2D(index_mode=1)
-#     plt.close()
+def test_plot_modes_4():
+    dmd = RDMD()
+    snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
+    dmd.fit(X=snapshots)
+    dmd.plot_modes_2D(index_mode=1)
+    plt.close()
 
-# def test_plot_modes_5():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
-#     dmd.fit(X=snapshots)
-#     dmd.plot_modes_2D(index_mode=1, filename='tmp.png')
-#     os.remove('tmp.1.png')
+def test_plot_modes_5():
+    dmd = RDMD()
+    snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
+    dmd.fit(X=snapshots)
+    dmd.plot_modes_2D(index_mode=1, filename='tmp.png')
+    os.remove('tmp.1.png')
 
-# def test_plot_snapshots_1():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     with raises(ValueError):
-#         dmd.plot_snapshots_2D()
+def test_plot_snapshots_1():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    with raises(ValueError):
+        dmd.plot_snapshots_2D()
 
-# def test_plot_snapshots_2():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD(svd_rank=-1)
-#     dmd.fit(X=sample_data)
-#     dmd.plot_snapshots_2D((1, 2, 5), x=np.arange(20), y=np.arange(20))
-#     plt.close()
+def test_plot_snapshots_2():
+    dmd = RDMD(svd_rank=-1)
+    dmd.fit(X=sample_data)
+    dmd.plot_snapshots_2D((1, 2, 5), x=np.arange(20), y=np.arange(20))
+    plt.close()
 
-# def test_plot_snapshots_3():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
-#     dmd.fit(X=snapshots)
-#     dmd.plot_snapshots_2D()
-#     plt.close()
+def test_plot_snapshots_3():
+    dmd = RDMD()
+    snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
+    dmd.fit(X=snapshots)
+    dmd.plot_snapshots_2D()
+    plt.close()
 
-# def test_plot_snapshots_4():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
-#     dmd.fit(X=snapshots)
-#     dmd.plot_snapshots_2D(index_snap=2)
-#     plt.close()
+def test_plot_snapshots_4():
+    dmd = RDMD()
+    snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
+    dmd.fit(X=snapshots)
+    dmd.plot_snapshots_2D(index_snap=2)
+    plt.close()
 
-# def test_plot_snapshots_5():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
-#     dmd.fit(X=snapshots)
-#     dmd.plot_snapshots_2D(index_snap=2, filename='tmp.png')
-#     os.remove('tmp.2.png')
+def test_plot_snapshots_5():
+    dmd = RDMD()
+    snapshots = [snap.reshape(20, 20) for snap in sample_data.T]
+    dmd.fit(X=snapshots)
+    dmd.plot_snapshots_2D(index_snap=2, filename='tmp.png')
+    os.remove('tmp.2.png')
 
-# def test_tdmd_plot():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD(tlsq_rank=3)
-#     dmd.fit(X=sample_data)
-#     dmd.plot_eigs(show_axes=False, show_unit_circle=False)
-#     plt.close()
+def test_tdmd_plot():
+    dmd = RDMD(tlsq_rank=3)
+    dmd.fit(X=sample_data)
+    dmd.plot_eigs(show_axes=False, show_unit_circle=False)
+    plt.close()
 
 # TODO: Create an equivalent test for RDMD
 # def test_cdmd_matrix_uniform():
@@ -219,104 +199,93 @@ def test_dmd_time_1():
 #     error_norm = np.linalg.norm(dmd.reconstructed_data - sample_data, 1)
 #     assert error_norm < 1e-10
 
-# def test_sorted_eigs_default():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     assert dmd.operator._sorted_eigs == False
+def test_sorted_eigs_default():
+    dmd = RDMD()
+    assert dmd.operator._sorted_eigs == False
 
-# def test_sorted_eigs_param():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD(sorted_eigs='real')
-#     assert dmd.operator._sorted_eigs == 'real'
+def test_sorted_eigs_param():
+    dmd = RDMD(sorted_eigs='real')
+    assert dmd.operator._sorted_eigs == 'real'
 
-# def test_get_bitmask_default():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     assert np.all(dmd.modes_activation_bitmask == True)
+def test_get_bitmask_default():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    assert np.all(dmd.modes_activation_bitmask == True)
 
-# def test_set_bitmask():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
+def test_set_bitmask():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
 
-#     new_bitmask = np.full(len(dmd.amplitudes), True, dtype=bool)
-#     new_bitmask[[0]] = False
-#     dmd.modes_activation_bitmask = new_bitmask
+    new_bitmask = np.full(len(dmd.amplitudes), True, dtype=bool)
+    new_bitmask[[0]] = False
+    dmd.modes_activation_bitmask = new_bitmask
 
-#     assert dmd.modes_activation_bitmask[0] == False
-#     assert np.all(dmd.modes_activation_bitmask[1:] == True)
+    assert dmd.modes_activation_bitmask[0] == False
+    assert np.all(dmd.modes_activation_bitmask[1:] == True)
 
-# def test_not_fitted_get_bitmask_raises():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     with raises(RuntimeError):
-#         print(dmd.modes_activation_bitmask)
+def test_not_fitted_get_bitmask_raises():
+    dmd = RDMD()
+    with raises(RuntimeError):
+        print(dmd.modes_activation_bitmask)
 
-# def test_not_fitted_set_bitmask_raises():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     with raises(RuntimeError):
-#         dmd.modes_activation_bitmask = np.full(3, True, dtype=bool)
+def test_not_fitted_set_bitmask_raises():
+    dmd = RDMD()
+    with raises(RuntimeError):
+        dmd.modes_activation_bitmask = np.full(3, True, dtype=bool)
 
-# def test_raise_wrong_dtype_bitmask():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     with raises(RuntimeError):
-#         dmd.modes_activation_bitmask = np.full(3, 0.1)
+def test_raise_wrong_dtype_bitmask():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    with raises(RuntimeError):
+        dmd.modes_activation_bitmask = np.full(3, 0.1)
 
-# def test_fitted():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     assert not dmd.fitted
-#     dmd.fit(X=sample_data)
-#     assert dmd.fitted
+def test_fitted():
+    dmd = RDMD()
+    assert not dmd.fitted
+    dmd.fit(X=sample_data)
+    assert dmd.fitted
 
-# def test_bitmask_amplitudes():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
+def test_bitmask_amplitudes():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
 
-#     old_n_amplitudes = dmd.amplitudes.shape[0]
-#     retained_amplitudes = np.delete(dmd.amplitudes, [0,-1])
+    old_n_amplitudes = dmd.amplitudes.shape[0]
+    retained_amplitudes = np.delete(dmd.amplitudes, [0,-1])
 
-#     new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
-#     new_bitmask[[0,-1]] = False
-#     dmd.modes_activation_bitmask = new_bitmask
+    new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
+    new_bitmask[[0,-1]] = False
+    dmd.modes_activation_bitmask = new_bitmask
 
-#     assert dmd.amplitudes.shape[0] == old_n_amplitudes - 2
-#     np.testing.assert_almost_equal(dmd.amplitudes, retained_amplitudes)
+    assert dmd.amplitudes.shape[0] == old_n_amplitudes - 2
+    np.testing.assert_almost_equal(dmd.amplitudes, retained_amplitudes)
 
-# def test_bitmask_eigs():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
+def test_bitmask_eigs():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
 
-#     old_n_eigs = dmd.eigs.shape[0]
-#     retained_eigs = np.delete(dmd.eigs, [0,-1])
+    old_n_eigs = dmd.eigs.shape[0]
+    retained_eigs = np.delete(dmd.eigs, [0,-1])
 
-#     new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
-#     new_bitmask[[0,-1]] = False
-#     dmd.modes_activation_bitmask = new_bitmask
+    new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
+    new_bitmask[[0,-1]] = False
+    dmd.modes_activation_bitmask = new_bitmask
 
-#     assert dmd.eigs.shape[0] == old_n_eigs - 2
-#     np.testing.assert_almost_equal(dmd.eigs, retained_eigs)
+    assert dmd.eigs.shape[0] == old_n_eigs - 2
+    np.testing.assert_almost_equal(dmd.eigs, retained_eigs)
 
-# def test_bitmask_modes():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
+def test_bitmask_modes():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
 
-#     old_n_modes = dmd.modes.shape[1]
-#     retained_modes = np.delete(dmd.modes, [0,-1], axis=1)
+    old_n_modes = dmd.modes.shape[1]
+    retained_modes = np.delete(dmd.modes, [0,-1], axis=1)
 
-#     new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
-#     new_bitmask[[0,-1]] = False
-#     dmd.modes_activation_bitmask = new_bitmask
+    new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
+    new_bitmask[[0,-1]] = False
+    dmd.modes_activation_bitmask = new_bitmask
 
-#     assert dmd.modes.shape[1] == old_n_modes - 2
-#     np.testing.assert_almost_equal(dmd.modes, retained_modes)
+    assert dmd.modes.shape[1] == old_n_modes - 2
+    np.testing.assert_almost_equal(dmd.modes, retained_modes)
 
 def test_getitem_modes():
     dmd = RDMD(svd_rank=10)
@@ -343,36 +312,33 @@ def test_getitem_modes():
 
     assert dmd.modes.shape[1] == old_n_modes
 
-# def test_getitem_raises():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
+def test_getitem_raises():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
 
-#     with raises(ValueError):
-#         dmd[[0,1,1,0,1]]
-#     with raises(ValueError):
-#         dmd[[True, True, False, True]]
-#     with raises(ValueError):
-#         dmd[1.0]
+    with raises(ValueError):
+        dmd[[0,1,1,0,1]]
+    with raises(ValueError):
+        dmd[[True, True, False, True]]
+    with raises(ValueError):
+        dmd[1.0]
 
-# def test_reconstructed_data():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
+def test_reconstructed_data():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
 
-#     new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
-#     new_bitmask[[0,-1]] = False
-#     dmd.modes_activation_bitmask = new_bitmask
+    new_bitmask = np.full(dmd.amplitudes.shape[0], True, dtype=bool)
+    new_bitmask[[0,-1]] = False
+    dmd.modes_activation_bitmask = new_bitmask
 
-#     dmd.reconstructed_data
-#     assert True
+    dmd.reconstructed_data
+    assert True
 
 # this is a test for the correctness of the amplitudes saved in the Proxy
 # between DMDBase and the modes activation bitmask. if this test fails
 # you probably need to call allocate_proxy once again after you compute
 # the final value of the amplitudes
-# def test_correct_amplitudes():
-#     # TODO: Implement svd_rank functionality
-#     dmd = RDMD()
-#     dmd.fit(X=sample_data)
-#     np.testing.assert_array_almost_equal(dmd.amplitudes, dmd._b)
+def test_correct_amplitudes():
+    dmd = RDMD()
+    dmd.fit(X=sample_data)
+    np.testing.assert_array_almost_equal(dmd.amplitudes, dmd._b)
