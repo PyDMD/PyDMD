d1fe8db03328cb631ba81b8c55c2c1ea92eed2e4
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-01-07 15:08:42 +0100

    fix tests

--- tests/test_tensorized.py
+++ tests/test_tensorized.py
@@ -27,11 +27,27 @@ dmds = [
 @pytest.mark.parametrize("dmd", dmds)
 @pytest.mark.parametrize("X", torch_backends)
 def test_tensorized_snapshots(dmd, X):
+    if isinstance(dmd, (HankelDMD, HODMD)):
+        pytest.skip()
     X = torch.stack([X * i for i in range(1, 11)])
     dmd.fit(X=X)
     assert dmd._snapshots.shape == X.shape
     assert_allclose(dmd._snapshots[0], X[0])
 
+@pytest.mark.parametrize("X", torch_backends)
+def test_tensorized_snapshots_hankeldmd(X):
+    X = torch.stack([X * i for i in range(1, 11)])
+    dmd = HankelDMD(svd_rank=-1, d=3)
+    dmd.fit(X=X)
+    assert dmd.snapshots.shape == (10, 3 * X.shape[-2], X.shape[-1] - 2)
+
+@pytest.mark.parametrize("X", torch_backends)
+def test_tensorized_snapshots_hodmd(X):
+    X = torch.stack([X * i for i in range(1, 11)])
+    dmd = HODMD(svd_rank=-1, svd_rank_extra=-1, d=3)
+    dmd.fit(X=X)
+    assert dmd.snapshots.shape[0] == 10
+    assert dmd.snapshots.shape[-1] == X.shape[-1]
 
 @pytest.mark.parametrize("dmd", dmds)
 @pytest.mark.parametrize("X", torch_backends)
