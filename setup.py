ca9c7eec55bedf00febd18588e719831da545a23
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-01-16 20:32:48 +0100

    reduce requirements on ezyrb

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ REQUIRED = [
 
 EXTRAS = {
     'docs': ['Sphinx==1.4', 'sphinx_rtd_theme'],
-    'test': ['pytest', 'pytest-cov', 'ezyrb>=1.3.0.post2301'],
+    'test': ['pytest', 'pytest-cov', 'ezyrb>=v1.2.1.post2205'],
 }
 
 LDESCRIPTION = (
