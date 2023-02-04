bf00cb60fb06cf17965faed526b8ba8e2ce3605f
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-02-04 01:49:27 +0100

    getsvda

--- pydmd/linalg/pytorch_linalg.py
+++ pydmd/linalg/pytorch_linalg.py
@@ -317,6 +317,8 @@ class LinalgPyTorch(LinalgBase):
     def svd(cls, X, *args, **kwargs):
         import torch
 
+        if X.device.type == "cuda":
+            kwargs["driver"] = "gesvda"
         return torch.linalg.svd(X, *args, **kwargs)
 
     @classmethod
