0e12e1b62b9d97e62ab77a9c18e001b7e7a6e28a
Nicola Demo <demo.nicola@gmail.com>
2023-02-21 08:59:21 +0100

    Update info2json.py

--- utils/info2json.py
+++ utils/info2json.py
@@ -1,20 +1,8 @@
-import json
-
 config = {
-    "testing_strategy_ghact": {
-        "fail-faste": "false",
-        "python-version":  
-            [
-                "3.8",    
-                "3.9"
-            ],
-        "os":
-            [
-                "windows-latest",                                 
-                "macos-latest",                 
-                "ubuntu-latest" 
-            ]    
-    }       
-}   
-
-print(json.dumps(config['testing_strategy_ghact']))
+    'os': [
+        "windows-latest",
+        "macos-latest",
+        "ubuntu-latest"
+    ]
+}
+print(json.dumps(config))
