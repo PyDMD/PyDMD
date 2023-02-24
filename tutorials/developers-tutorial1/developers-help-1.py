02dc03594a41f2de90d001ac94693658e1ee5d3e
fAndreuzzi <fAndreuzzi@users.noreply.github.com>
2023-02-24 08:42:42 +0000

    export tutorials changed in 2cda428

--- tutorials/developers-tutorial1/developers-help-1.py
+++ tutorials/developers-tutorial1/developers-help-1.py
@@ -6,21 +6,21 @@
 # ## Developers Tutorial 1: Extending PyDMD
 
 # Contrary to other **PyDMD** tutorials, this notebook illustrates the general structure of the basic classes, aiming to help developers to <u>extend</u> the package functionality in a smooth way.
-#
+# 
 # In this tutorial we will indeed create from scratch a new version of the original DMD; such extension will be totally useless from a scientific perspective, the only purpose is showing the suggested steps to implement new variants inside **PyDMD**.
 
 # The necessary bricks for building the new DMD version are:
-#
+# 
 # - [`DMDBase`](https://mathlab.github.io/PyDMD/dmdbase.html), the actual backbone of all the different implemented versions;
 # - `DMDTimeDict`, the class that manages the time window;
 # - `DMDOperator`, the class that manages the so-called DMD operator;
-#
+# 
 # We start the new module by importing all these classes and the usual math environment (`matplotlib`+`numpy`).
 
 # In[1]:
 
 
-get_ipython().run_line_magic("matplotlib", "inline")
+get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -32,12 +32,12 @@ import matplotlib.colors as colors
 
 
 # We are now able to create a new class inheriting from `DMDBase`: in this way we just need to implement the new `fit()` method. We can also overload one or more attributes/methods, depending on the algorithm which we are going to follow.
-#
-# In this case, we also overload the constructor (i.e. the `__init__` method) to present a non-trivial case.
-#
+# 
+# In this case, we also overload the constructor (i.e. the `__init__` method) to present a non-trivial case. 
+# 
 # ### Our simple algorithm
 # The new functionality we are going to implement is basically a simple DMD which maps the input snapshots using a custom function, provided by the user before of the construction of the operator.
-#
+# 
 # Since the (quite) high number of parameters in the constructor, we suggest to pass explicitly all of them instead of using `args` and `kwargs`. It's redundant, but also much more readable! We can call the `DMDBase` constructor thanks to `super` keyword (more info on `super` [here](https://docs.python.org/3.9/library/functions.html?highlight=super#super)).
 
 # In[2]:
@@ -57,7 +57,7 @@ class MyFancyNameDMD(DMDBase):
 
 
 # We just specify that the last line of constructor saves the passed function in the object, giving us the possibility to recall it later.
-#
+# 
 # We are now ready to implement the `fit` method. Define the custom mapping as $f: \mathbb R^n \to \mathbb R^n$ acting on the snapshots $\{\mathbf x_i \in \mathbb R^n\}_{i=1}^m$. Then we just need to pre-process the snapshots in `fit()` and build the DMD operator.
 
 # In[3]:
@@ -99,7 +99,7 @@ def fit(self, X):
 
 
 # By recalling the already implemented methods, we just need few lines of code to complete the new version. Of course, in case of doubts, the [documentation](https://mathlab.github.io/PyDMD/index.html) is the best starting point to better understand classes and methods.
-#
+# 
 # We merge the two cells above in the next one in order to have the entire class implemented.
 
 # In[4]:
@@ -155,7 +155,7 @@ class MyFancyNameDMD(DMDBase):
 
 
 # And, we're done!
-#
+# 
 # Our new class is implemented, inheriting all the methods to the base class. We have nothing to do but try it on a simple example. First of all, we instantiate a new object, passing as function a simple normalizer.
 
 # In[5]:
@@ -237,8 +237,8 @@ plt.show()
 
 
 # Reconstruction looks fine! We are able to note that, since the normalization of the snapshots, the two approaches returns similar results, just a bit rescaled, that is what we expected by preprocessing in this way the input data.
-#
+# 
 # But what about if we want to rescale back the reconstruction (or the modes) in the new version?
-# Simple, we have just to overload the `reconstructed_data` or `modes` attributes.
-#
+# Simple, we have just to overload the `reconstructed_data` or `modes` attributes. 
+# 
 # This is the basic guide to implement a new version of DMD. For more complex changes to the algorithm, we recommend to carefully read the documentation of the package, and maybe reporting in the [Github issues page](https://github.com/mathLab/PyDMD/issues) the extension you are going to implement.
