34ea0033b2112155d6a6326f7b34a648fd3cf27b
Nicola Demo <demo.nicola@gmail.com>
2023-07-17 10:08:11 +0200

    add check for `_rescale_mode` attr

--- pydmd/dmdoperator.py
+++ pydmd/dmdoperator.py
@@ -90,8 +90,10 @@ class DMDOperator:
                 msg = "Casting atilde from np.complex256 to np.complex128"
                 logging.info(msg)
 
-        if self._rescale_mode == "auto":
+        if (isinstance(self._rescale_mode, str) and
+                self._rescale_mode == "auto"):
             self._rescale_mode = s
+
 
         self._Atilde = atilde
         self._compute_eigenquantities()
