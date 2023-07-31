f6af691be685e59db6e7e787d5fab89323d02721
klapo <lapo.karl@gmail.com>
2023-07-31 13:37:08 +0200

    Refactored CostsDMD -> COSTS. Removed redundant definitions.

--- pydmd/costs.py
+++ pydmd/costs.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import xarray as xr
 
 
-class CostsDMD:
+class COSTS:
     """Coherent Spatio-Temporal Scale Separation with DMD.
 
     :param window_length: Length of the analysis window in number of time steps.
@@ -80,8 +80,6 @@ class CostsDMD:
         n_components=None,
     ):
         self._n_components = n_components
-        self._step_size = step_size
-        self._window_length = window_length
         self._svd_rank = svd_rank
         self._global_svd = global_svd
         self._initialize_artificially = initialize_artificially
