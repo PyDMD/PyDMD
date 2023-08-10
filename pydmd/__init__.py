02d6234b52dd7d7084619c435e68c8d1aed87679
sichinaga <misara1999@gmail.com>
2023-08-10 12:05:56 -0700

    Initial commit

--- pydmd/__init__.py
+++ pydmd/__init__.py
@@ -15,6 +15,7 @@ __all__ = [
     "havok",
     "bopdmd",
     "pidmd",
+    "edmd",
 ]
 
 
@@ -36,3 +37,4 @@ from .rdmd import RDMD
 from .havok import HAVOK
 from .bopdmd import BOPDMD
 from .pidmd import PiDMD
+from .edmd import EDMD
