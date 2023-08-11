47869c996efb2ce8622758e7afeb985be6478c89
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-08-11 22:14:42 +0200

    dunnoh

---
+++ tests/linalg/test_pseudo_hankel_matrix.py
@@ -0,0 +1,17 @@
+from pydmd.linalg import build_linalg_module
+from .utils import assert_allclose, setup_backends, Backend
+import pytest
+from functools import partial
+
+torch_backends = setup_backends(exclude=Backend.NUMPY)
+
+
+@pytest.mark.parametrize("X", torch_backends)
+def test_tensorized_pseudo_hankel_matrix(X):
+    linalg_module = build_linalg_module(X)
+    X = linalg_module.new_array([X * i for i in range(1, 11)])
+    phm = partial(linalg_module.pseudo_hankel_matrix, d=5)
+    assert_allclose(
+        phm(X),
+        list(map(phm, X)),
+    )
