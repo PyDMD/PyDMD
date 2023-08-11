d01020e9ddeafa1fe463ee51af6eb9faf71496dd
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-08-11 22:32:15 +0200

    test

--- tests/test_pre_post_processing.py
+++ tests/test_pre_post_processing.py
@@ -1,4 +1,8 @@
-from pydmd import PrePostProcessingDMD
+from pydmd.pre_post_processing import (
+    PrePostProcessingDMD,
+    zero_mean_preprocessing,
+)
+import numpy as np
 
 
 def test_pre_processing(mocker):
@@ -21,3 +25,17 @@ def test_pre_processing(mocker):
 
     assert pdmd.reconstructed_data is output
     post.assert_called_once_with(partial_output, preproc_state)
+
+
+def test_zero_mean(mocker):
+    dmd = mocker.Mock()
+    pdmd = zero_mean_preprocessing(dmd)
+
+    X = np.array([1, 2, 3], dtype=float)
+    pdmd.fit(X)
+    fit_call_args = dmd.fit.call_args_list
+    assert len(fit_call_args) == 1
+    assert (fit_call_args[0][0][0] == [-1, 0, 1]).all()
+
+    dmd.reconstructed_data = np.zeros(3, dtype=float)
+    assert (pdmd.reconstructed_data == [2, 2, 2]).all()
