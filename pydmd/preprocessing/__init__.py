d914c3f7e877a07ddc653567d685afd670120707
fAndreuzzi <andreuzzi.francesco@gmail.com>
2023-08-15 00:32:03 +0200

    missing import

--- pydmd/preprocessing/__init__.py
+++ pydmd/preprocessing/__init__.py
@@ -5,3 +5,4 @@ PyDMD preprocessing init.
 from .pre_post_processing import PrePostProcessingDMD
 from .zero_mean import zero_mean_preprocessing
 from .hankel import hankel_preprocessing
+from .svd_projection import svd_projection_preprocessing
