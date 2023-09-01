c69fea14f2d49b5de14d242d1130b9200b8a5167
sichinaga <misara1999@gmail.com>
2023-09-01 14:22:49 -0700

    Major documentation contributions

--- pydmd/lando.py
+++ pydmd/lando.py
@@ -3,7 +3,10 @@ Derived module from dmdbase.py for linear and
 nonlinear disambiguation optimization (LANDO).
 
 References:
-- 
+- Baddoo Peter J., Herrmann Benjamin, McKeon Beverley J. and Brunton Steven L.
+2022 Kernel learning for robust dynamic mode decomposition: linear and
+nonlinear disambiguation optimization. Proc. R. Soc. A. 478: 20210830.
+20210830. http://doi.org/10.1098/rspa.2021.0830
 """
 import warnings
 import numpy as np
@@ -12,18 +15,32 @@ from sklearn.metrics.pairwise import pairwise_kernels
 
 from .dmdbase import DMDBase
 from .dmdoperator import DMDOperator
-from .utils import compute_svd, compute_tlsq
 from .snapshots import Snapshots
+from .utils import compute_svd, compute_tlsq
 
-SUPPORTED_KERNELS = ["poly", "rbf", "linear"]
-DEFAULT_DEGREE = 3
-DEFAULT_COEF0 = 1.0
-DEFAULT_GAMMA = 1.0
+SUPPORTED_KERNELS = ["linear", "poly", "rbf"]
 
 
 class LANDOOperator(DMDOperator):
     """
-    LANDO operator.
+    LANDO operator class, which is used to compute and keep track of the
+    dictionary-based kernel model and the diagnostics of the linear model
+    about a fixed point.
+
+    :param svd_rank: the rank for the truncation of the linear operator; If 0,
+        the method computes the optimal rank and uses it for truncation; if a
+        positive integer, the method uses the argument for the truncation; if
+        float between 0 and 1, the rank is the number of the biggest singular
+        values that are needed to reach the 'energy' specified by `svd_rank`;
+        if -1, the method does not compute a truncation.
+    :type svd_rank: int or float
+    :param kernel_metric: the kernel function to apply. Supported kernel
+        metrics for `LANDO` are `"linear"`, `"poly"`, and `"rbf"`.
+    :type kernel_metric: str
+    :param kernel_params: additional parameters to be inputted to the
+        `sklearn.metrics.pairwise_kernels` function. This includes
+        kernel-specific function parameters.
+    :type kernel_params: dict
     """
 
     def __init__(
@@ -50,62 +67,135 @@ class LANDOOperator(DMDOperator):
         self._A = None
 
     def kernel_function(self, X, Y):
+        """
+        Calls `sklearn.metrics.pairwise_kernels` to evaluate the kernel
+        function at the given feature matrices X and Y.
+
+        :param X: feature matrix with shape (n_features, n_samples_X)
+        :type X: numpy.ndarray
+        :param Y: second feature matrix with shape (n_features, n_samples_Y)
+        :type Y: numpy.ndarray
+        :return: kernel matrix with shape (n_samples_X, n_samples_Y)
+        :rtype: numpy.ndarray
+        """
         return pairwise_kernels(
             X.T, Y.T, metric=self._kernel_metric, **self._kernel_params
         )
 
     def kernel_gradient(self, X, y):
-        if self._kernel_metric == "poly":
+        """
+        Computes the gradient of the kernel with respect to the second feature
+        vector x. Then evaluates the gradient at the feature matrix X and the
+        feature vector x = y. Currently, this method is only compatible with
+        the linear, polynomial, and RBF kernels.
+
+        :param X: feature matrix with shape (n_features, n_samples_X)
+        :type X: numpy.ndarray
+        :param y: feature vector with shape (n_features,)
+        :type y: numpy.ndarray
+        :return: kernel gradient matrix with shape (n_samples_X, n_features)
+        :rtype: numpy.ndarray
+        """
+        if self._kernel_metric == "linear":
+            return X.T
+
+        # Kernel metric must be polynomial or RBF.
+        if "gamma" in self._kernel_params.keys():
             gamma = self._kernel_params["gamma"]
+        else: # set the pairwise_kernels gamma default
+            gamma = 1.0 / X.shape[0]
+
+        if self._kernel_metric == "poly":
             coef0 = self._kernel_params["coef0"]
             degree = self._kernel_params["degree"]
             return np.diag(
                 gamma * degree * (coef0 + gamma * X.T.dot(y)) ** (degree - 1)
             ).dot(X.T)
 
-        if self._kernel_metric == "rbf":
-            gamma = self._kernel_params["gamma"]
-            centered_X = X - y[:, None]
-            return np.diag(
-                -2
-                * gamma
-                * np.exp(-gamma * np.linalg.norm(centered_X, 2, axis=0) ** 2)
-            ).dot(centered_X.T)
-
-        return X.T
+        # Kernel metric is RBF.
+        centered_X = X - y[:, None]
+        return np.diag(
+            -2
+            * gamma
+            * np.exp(-gamma * np.linalg.norm(centered_X, 2, axis=0) ** 2)
+        ).dot(centered_X.T)
 
     @property
     def weights(self):
+        """
+        Get the dictionary-based kernel model weights.
+        Referred to as W_tilde in the original reference.
+
+        :return: the kernel model weights.
+        :rtype: numpy.ndarray
+        """
         if self._weights is None:
             raise ValueError("You need to call fit.")
         return self._weights
 
     @property
     def eigenvalues(self):
+        """
+        Get the eigenvalues of the full linear operator evaluated about a fixed
+        point. This operator is referred to as L in the original reference.
+
+        :return: the eigenvalues of the full linear operator.
+        :rtype: numpy.ndarray
+        """
         if self._eigenvalues is None:
             raise ValueError("You need to call fit and analyze_fixed_point.")
         return self._eigenvalues
 
     @property
     def eigenvectors(self):
+        """
+        Get the eigenvectors of the reduced linear operator evaluated about a
+        fixed point. This operator is referred to as L_hat in the original
+        reference.
+
+        :return: the eigenvectors of the reduced linear operator.
+        :rtype: numpy.ndarray
+        """
         if self._eigenvectors is None:
             raise ValueError("You need to call fit and analyze_fixed_point.")
         return self._eigenvectors
 
     @property
     def modes(self):
+        """
+        Get the eigenvectors of the full linear operator evaluated about a
+        fixed point. This operator is referred to as L in the original
+        reference.
+
+        :return: the eigenvectors of the full linear operator.
+        :rtype: numpy.ndarray
+        """
         if self._modes is None:
             raise ValueError("You need to call fit and analyze_fixed_point.")
         return self._modes
 
     @property
     def as_numpy_array(self):
+        """
+        Get the reduced linear operator A_tilde.
+        Referred to as L_hat in the original reference.
+
+        :return: the reduced linear operator A_tilde.
+        :rtype: numpy.ndarray
+        """
         if self._Atilde is None:
             raise ValueError("You need to call fit and analyze_fixed_point.")
         return self._Atilde
 
     @property
     def A(self):
+        """
+        Get the full linear operator A.
+        Referred to as L in the original reference.
+
+        :return: the full linear operator A.
+        :rtype: numpy.ndarray
+        """
         if self._A is None:
             msg = (
                 "Full linear operator currently not computed. You may need to "
@@ -116,9 +206,31 @@ class LANDOOperator(DMDOperator):
         return self._A
 
     def compute_operator(self, X, Y, X_dict):
+        """
+        Compute the dictionary-based kernel model weights.
+
+        :param X: the input snapshots x.
+        :type X: numpy.ndarray
+        :param Y: input snapshots y such that F(x) = y.
+        :type Y: numpy.ndarray
+        :param X_dict: the sparse feature dictionary.
+        :type X_dict: numpy.ndarray
+        """
         self._weights = Y.dot(np.linalg.pinv(self.kernel_function(X_dict, X)))
 
     def compute_linear_operator(self, fixed_point, compute_A, X_dict):
+        """
+        Compute the DMD diagnostics of the linear model about a fixed point.
+
+        :param fixed_point: base state about which to perturb the system.
+        :type fixed_point: numpy.ndarray
+        :param compute_A: if True, the full linear operator is explicitly
+            computed and stored. If False, the full linear operator isn't
+            computed and stored explicitly.
+        :type compute_A: bool
+        :param X_dict: the sparse feature dictionary.
+        :type X_dict: numpy.ndarray
+        """
         kernel_grad = self.kernel_gradient(X_dict, fixed_point)
         U, s, V = compute_svd(kernel_grad.T, self._svd_rank)
         if compute_A:
@@ -126,7 +238,7 @@ class LANDOOperator(DMDOperator):
             self._Atilde = np.linalg.multi_dot([U.conj().T, self._A, U])
         else:
             self._Atilde = np.linalg.multi_dot(
-                [U.conj().T, self.weights.dot(kernel_grad), U]
+                [U.conj().T, self.weights, kernel_grad, U]
             )
 
         # Filter out zero eigenvalues.
@@ -172,10 +284,10 @@ class LANDO(DMDBase):
         between 0 and 1 may give better results. Default is False.
     :type opt: bool or int
     :param kernel_metric: the kernel function to apply. Supported kernel
-        metrics are `"poly"`, `"rbf"`, and `"linear"`.
+        metrics for `LANDO` are `"linear"`, `"poly"`, and `"rbf"`.
     :type kernel_metric: str
-    :param kernel_params: additional parameters for the
-        `sklearn.metrics.pairwise_kernels` function, including
+    :param kernel_params: additional parameters to be inputted to the
+        `sklearn.metrics.pairwise_kernels` function. This includes
         kernel-specific function parameters.
     :type kernel_params: dict
     :param dict_tol: threshold at which delta_t, the degree to which a snapshot
@@ -202,15 +314,14 @@ class LANDO(DMDBase):
     ):
         self._test_kernel_inputs(kernel_metric, kernel_params)
 
-        # Set the default kernel parameterizations.
         if kernel_params is None:
             kernel_params = {}
-        if kernel_metric != "linear" and "gamma" not in kernel_params.keys():
-            kernel_params["gamma"] = DEFAULT_GAMMA
+        # set the pairwise_kernels polynomial degree default
         if kernel_metric == "poly" and "degree" not in kernel_params.keys():
-            kernel_params["degree"] = DEFAULT_DEGREE
+            kernel_params["degree"] = 3
+        # set the pairwise_kernels polynomial coef0 default
         if kernel_metric == "poly" and "coef0" not in kernel_params.keys():
-            kernel_params["coef0"] = DEFAULT_COEF0
+            kernel_params["coef0"] = 1
 
         super().__init__(
             svd_rank=svd_rank,
@@ -230,7 +341,7 @@ class LANDO(DMDBase):
         self._dict_tol = dict_tol
         self._permute = permute
 
-        # Keep track of the results of the last fixed point analysis.
+        # Keep track of the last fixed point analysis.
         self._fixed_point = None
         self._bias = None
 
@@ -251,18 +362,40 @@ class LANDO(DMDBase):
 
     @property
     def sparse_dictionary(self):
+        """
+        Get the learned sparse feature dictionary.
+
+        :return: the sparse feature dictionary.
+        :rtype: numpy.ndarray
+        """
         if not self.partially_fitted:
             warnings.warn("You need to call fit.")
         return self._sparse_dictionary
 
     @property
     def fixed_point(self):
+        """
+        Get the fixed point from the last fixed point analysis.
+
+        :return: the fixed point from the last fixed point analysis.
+        :rtype: numpy.ndarray
+        """
         if not self.fitted:
             warnings.warn("You need to call fit and analyze_fixed_point.")
         return self._fixed_point
 
     @property
     def bias(self):
+        """
+        Get the bias term from the last fixed point analysis. This is in
+        reference to the term c in the expression f(x) = c + Ax' + N(x') for
+        our system perturbed about a base state x_bar where x = x_bar + x'.
+        f is our model approximation, A is a linear operator, and N is a
+        nonlinear operator.
+
+        :return: the bias term from the last fixed point analysis.
+        :rtype: numpy.ndarray
+        """
         if not self.fitted:
             warnings.warn("You need to call fit and analyze_fixed_point.")
         return self._bias
@@ -271,9 +404,9 @@ class LANDO(DMDBase):
         """
         Prediction of the true system model F, where F(x) = y.
 
-        :param x:
+        :param x: feature vector from the data domain.
         :type x: numpy.ndarray
-        :return:
+        :return: f(x), an approximation of the true quantity F(x).
         :rtype: numpy.ndarray
         """
         if not self.partially_fitted:
@@ -284,6 +417,19 @@ class LANDO(DMDBase):
         )
 
     def nonlinear(self, x):
+        """
+        Get the nonlinear term from the last fixed point analysis, evaluated
+        at the input vector x. This is in reference to the term N(x') in the
+        expression f(x) = c + Ax' + N(x') for our system perturbed about a
+        base state x_bar where x = x_bar + x'. f is our model approximation,
+        c is a constant bias term, and A is a linear operator.
+
+        :param x: feature vector from the data domain.
+        :type x: numpy.ndarray
+        :return: the nonlinear term from the last fixed point analysis,
+            evaluated at the input vector x.
+        :rtype: numpy.ndarray
+        """
         if not self.fitted:
             raise RuntimeError("You need to call fit and analyze_fixed_point.")
         x = self._check_input_shape(x)
@@ -307,8 +453,8 @@ class LANDO(DMDBase):
 
         :param X: the input snapshots.
         :type X: numpy.ndarray or iterable
-        :param Y: additional input snapshots such that F(x) = y.
-            If not provided, snapshots from X are used.
+        :param Y: additional input snapshots such that F(x) = y. If not given,
+            snapshots from X are used to build a discrete-time model.
         :type Y: numpy.ndarray or iterable
         """
         self._reset()
@@ -336,6 +482,16 @@ class LANDO(DMDBase):
         return self
 
     def analyze_fixed_point(self, fixed_point, compute_A=False):
+        """
+        Use a partially-fitted LANDO model to examine the system perturbed
+        about a given base state x_bar such that x = x_bar + x'. Extract the
+        bias term, linear portion, and nonlinear portion from the expression
+        f(x) = c + Ax' + N(x'), where f is our model approximation, c is a
+        constant bias term, A is a linear operator, and N is a nonlinear
+        operator. Additionally compute and obtain the DMD diagnostics of the
+        linear model A, along with the DMD amplitudes associated with the
+        computed linear model.
+        """
         if not self.partially_fitted:
             msg = (
                 "You need to call fit before analyzing "
@@ -352,7 +508,27 @@ class LANDO(DMDBase):
         )
         self._b = self._compute_amplitudes()
 
-    def predict(self, x0, tend, continuous=True, dt=1, solve_ivp_opts=None):
+    def predict(self, x0, tend, continuous=True, dt=1.0, solve_ivp_opts=None):
+        """
+        Reconstruct or predict the state of the system using the fitted model.
+
+        :param x0: initial condition from which to propagate.
+        :type x0: numpy.ndarray
+        :param tend: number of data points to compute.
+        :type tend: int
+        :param continuous: if True, the method assumes that the fitted model
+            f(x) is continuous, in which case f(x) predicts the time derivative
+            x'. If False, the method assumes that the fitted model is discrete,
+            in which case f(x_n) predicts the the future data point x_{n+1}.
+        :type continuous: bool
+        :param dt: desired time step between each computed data point. This
+            parameter is only used if `continuous=True`.
+        :type dt: float
+        :param solve_ivp_opts: dictionary of additional parameters to be passed
+            to the `scipy.integrate.solve_ivp` function. This parameter is only
+            used if `continuous=True`.
+        :type solve_ivp_opts: dict
+        """
         if not self.partially_fitted:
             raise RuntimeError("You need to call fit.")
 
@@ -363,11 +539,7 @@ class LANDO(DMDBase):
                 return self.f(x)
 
             if solve_ivp_opts is None:
-                # Set integrator keywords to replicate odeint defaults.
                 solve_ivp_opts = {}
-                solve_ivp_opts["rtol"] = 1e-12
-                solve_ivp_opts["method"] = "LSODA"
-                solve_ivp_opts["atol"] = 1e-12
 
             t_eval = np.arange(0, tend * dt, dt)
 
@@ -415,7 +587,9 @@ class LANDO(DMDBase):
             # Equation (3.11): Evaluate the kernel using the current dictionary
             # items and the next candidate addition to the dictionary.
             x_t = X[:, ind_t]
-            k_tilde_next = self.operator.kernel_function(X[:, dict_inds], x_t[:, None])
+            k_tilde_next = self.operator.kernel_function(
+                X[:, dict_inds], x_t[:, None]
+            )
 
             # Equation (3.10): Use backsubstitution to compute the span of the
             # current dictionary.
