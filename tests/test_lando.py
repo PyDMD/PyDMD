f5b8efe7c79ff9e81075ffd2032423645aa49a31
sichinaga <misara1999@gmail.com>
2023-10-09 14:27:43 -0700

    Test updates

--- tests/test_lando.py
+++ tests/test_lando.py
@@ -280,5 +280,5 @@ def test_online_3():
 
     assert_equal(lando_online.fixed_point, lando.fixed_point)
     assert np.linalg.norm(lando_online.bias) < 1e-3
-    assert relative_error(lando_online.linear, lando.linear) < 1e-6
-    assert relative_error(lando_online.nonlinear(X), lando.nonlinear(X)) < 1e-6
+    assert relative_error(lando_online.linear, lando.linear) < 1e-5
+    assert relative_error(lando_online.nonlinear(X), lando.nonlinear(X)) < 1e-5
