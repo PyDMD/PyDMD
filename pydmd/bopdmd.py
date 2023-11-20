859ff0df088119d93e2e15500f3170483dbc9dac
Sara <misara1999@gmail.com>
2023-11-20 09:46:51 -0800

    Change default number of bags

--- pydmd/bopdmd.py
+++ pydmd/bopdmd.py
@@ -948,7 +948,7 @@ class BOPDMD(DMDBase):
         init_alpha=None,
         proj_basis=None,
         num_trials=0,
-        trial_size=0.2,
+        trial_size=0.6,
         eig_sort="auto",
         eig_constraints=None,
         bag_warning=100,
