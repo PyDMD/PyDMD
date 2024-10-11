fix incorrect index generation to unscramble deltas

pydmd/bopdmd.py
@@ -624,8 +624,8 @@ class BOPDMDOperator(DMDOperator):
             q_out, djac_out, j_pvt = qr(
                 djac_matrix, mode="economic", pivoting=True
             )
-            ij_pvt = np.arange(IA)
-            ij_pvt = ij_pvt[j_pvt]
+            ij_pvt = np.zeros(IA).astype(int)
+            ij_pvt[j_pvt] = np.arange(IA).astype(int)
             rjac[:IA] = np.triu(djac_out[:IA])
             rhs_top = q_out.conj().T.dot(rhs_temp)
             scales_pvt = scales[j_pvt[:IA]]