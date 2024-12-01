5ba9f09bca2df845cb8281ab5a396d61f7b4e9ac
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2024-12-01 11:21:40 +0100

    use vrs supported on py3.8

--- utils/info2json.py
+++ utils/info2json.py
@@ -8,7 +8,7 @@ testing_matrix_ghact = {
     "python-version": ["3.8", "3.9", "3.10", "3.11", "3.12"],
     "os": ["windows-latest", macos_version, "ubuntu-latest"],
     "exclude": [{"os": "windows-latest", "python-version": "3.11"}],
-    "numpy-version": ["1.26.4", "2.0.2"],
+    "numpy-version": ["1.24.4", "2.0.2"],
 }
 
 tutorial_testing_matrix_ghact = {
