0a74bcf8c75d9d58ac80d22b355513b75ae612a2
ndem0 <ndem0@users.noreply.github.com>
2025-01-01 03:28:35 +0000

    deploy: 1f6db1f15a6d07b435c6e6d25387c21f8c891074

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2024.12.1',
+    VERSION: '2025.1.1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
