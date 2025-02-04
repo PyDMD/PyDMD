c7f0c4a5037a15bb16e7cc3987452dfe306f0aff
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-02-04 15:31:31 +0100

    Remove `NumPy<2` constraint (#560)
    * remove restriction
    
    * numpy tests
    
    * black
    
    * use vrs supported on py3.8
    
    * exclude 3.8 from npy 2.0.2
    
    * propagate exclude
    
    * exclude more
    
    * fix exclusion
    
    * exclude 1.26

--- utils/info2json.py
+++ utils/info2json.py
@@ -7,12 +7,25 @@ macos_version = "macos-13"
 testing_matrix_ghact = {
     "python-version": ["3.8", "3.9", "3.10", "3.11", "3.12"],
     "os": ["windows-latest", macos_version, "ubuntu-latest"],
-    "exclude": [{"os": "windows-latest", "python-version": "3.11"}],
+    "numpy-version": ["1.24.4", "1.26.4", "2.0.2"],
+    "exclude": [
+        {"os": "windows-latest", "python-version": "3.11"},
+        # 3.8 is only tested with NumPy 1.24.5
+        {"python-version": "3.8", "numpy-version": "1.26.4"},
+        {"python-version": "3.8", "numpy-version": "2.0.2"},
+        # NumPy 1.24.4 is only kept for 3.8
+        {"python-version": "3.9", "numpy-version": "1.24.4"},
+        {"python-version": "3.10", "numpy-version": "1.24.4"},
+        {"python-version": "3.11", "numpy-version": "1.24.4"},
+        {"python-version": "3.12", "numpy-version": "1.24.4"},
+    ],
 }
 
 tutorial_testing_matrix_ghact = {
-    "python-version": ["3.8", "3.9", "3.10", "3.11", "3.12"],
+    "python-version": testing_matrix_ghact["python-version"],
     "os": [macos_version, "ubuntu-latest"],
+    "numpy-version": testing_matrix_ghact["numpy-version"],
+    "exclude": testing_matrix_ghact["exclude"],
 }
 
 deploy_matrix_ghact = {"python-version": ["3.8"], "os": ["ubuntu-latest"]}
